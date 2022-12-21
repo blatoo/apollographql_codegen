158f379ec25edf150f69f30592e1ae532265bfd6
blatoo <connygy@googlemail.com>
2022-12-21 15:10:07 +0100

    changed resolver type to any

--- src/resolvers.ts
+++ src/resolvers.ts
@@ -2,28 +2,9 @@ import { DB_Company, DB_Job } from "./db"
 import { Company, Resolvers } from "./__generated__/resolvers-types"
 
 // if you set the type of resolvers as `Resolvers`, then it comes the type error.
-export const resolvers: Resolvers = {
-  Query: {
-    company: (_root, { id }) => DB_Company.findById(id),
-  },
-
-  Company: {
-    jobs: (company: Company) =>
-      DB_Job.findAll((job) => job.companyId === company.id),
-  },
-
-  Job: {
-    company: (job) => {
-      return DB_Company.findById(job.companyId)
-    },
-  },
-}
-
-
-// if you set the type of `resolvers` as any, then everything works fine.
-// export const resolvers: any = {
+// export const resolvers: Resolvers = {
 //   Query: {
-//     company: (_root: any, { id }: any) => DB_Company.findById(id),
+//     company: (_root, { id }) => DB_Company.findById(id),
 //   },
 
 //   Company: {
@@ -32,9 +13,28 @@ export const resolvers: Resolvers = {
 //   },
 
 //   Job: {
-//     company: (job: any) => {
+//     company: (job) => {
 //       return DB_Company.findById(job.companyId)
 //     },
 //   },
 // }
+
+
+// if you set the type of `resolvers` as any, then everything works fine.
+export const resolvers: any = {
+  Query: {
+    company: (_root: any, { id }: any) => DB_Company.findById(id),
+  },
+
+  Company: {
+    jobs: (company: Company) =>
+      DB_Job.findAll((job) => job.companyId === company.id),
+  },
+
+  Job: {
+    company: (job: any) => {
+      return DB_Company.findById(job.companyId)
+    },
+  },
+}
 
